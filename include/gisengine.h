adb13751845d700ce61467110aba4d655f7b3bad
Svilen M. Varbanov <svilen_varbanov@msn.com>
2024-07-02 19:10:37 +0300

    Build-2024-07-02_19-10-15

--- include/gisengine.h
+++ include/gisengine.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "engine.h"
+#include "../include/engine.h"
 
 // ************************************************************************************************
 #ifdef _WINDOWS
