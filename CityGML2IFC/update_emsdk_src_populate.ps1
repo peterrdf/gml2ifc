4af0f2242d0451e4dbdf2d9989b142ba3d164ba2
Svilen M. Varbanov <svilen_varbanov@msn.com>
2024-08-19 10:59:20 +0300

    PowerShell helper.

---
+++ CityGML2IFC/update_emsdk_src_populate.ps1
@@ -0,0 +1,5 @@
+Copy-Item -Path ".\pch.h" -Destination "D:\RDF\emsdk-main\src\gml2ifc\pch.h" -Force
+Copy-Item -Path ".\pch.cpp" -Destination "D:\RDF\emsdk-main\src\gml2ifc\pch.cpp" -Force
+Copy-Item -Path ".\_guid.h" -Destination "D:\RDF\emsdk-main\src\gml2ifc\_guid.h" -Force
+Copy-Item -Path ".\_gml2ifc.h" -Destination "D:\RDF\emsdk-main\src\gml2ifc\_gml2ifc.h" -Force
+Copy-Item -Path ".\_gml2ifc.cpp" -Destination "D:\RDF\emsdk-main\src\gml2ifc\_gml2ifc.cpp" -Force
