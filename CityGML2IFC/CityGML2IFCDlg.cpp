1c6db4f9c43e71830a7e77c145613625d341d72a
Svilen M. Varbanov <svilen_varbanov@msn.com>
2024-09-09 15:45:29 +0300

    Test.

--- CityGML2IFC/CityGML2IFCDlg.cpp
+++ CityGML2IFC/CityGML2IFCDlg.cpp
@@ -92,7 +92,10 @@ void CCityGML2IFCDlg::ExportFile(const wstring& strInputFile)
 	strOutputFile += L".ifc";
 
 	_gml2ifc_exporter exporter(m_strRootFolder, LogCallbackImpl, nullptr);
-	exporter.retrieveSRSData(strInputFile);  // TESTQQQQQQQQQQQQQ
+
+	// TEST
+	//exporter.retrieveSRSData(strInputFile);
+
 	exporter.execute(strInputFile, strOutputFile);
 }
 
